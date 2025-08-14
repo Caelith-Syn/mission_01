db1b07dd2e25d1e9a544605f4c97462a46aac13c
Caelith-Syn <ronan_o_murchu@hotmail.com>
2025-08-14 12:49:07 +1200

    STYLE: add logo to header and align brand group

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import { Link, NavLink } from "react-router-dom";
+import logo from "../assets/shield_auto_group_logo.svg";
 
 export default function Header() {
   return (
     <header className="header">
       <div className="container row">
-        <Link to="/" className="brand">
-          Shield Auto Group
+        <Link to="/" className="brand-wrap" aria-label="Shield Auto Group Home">
+          <img src={logo} alt="" className="brand-logo" />
         </Link>
         <nav className="nav">
           <NavLink to="/" end>
